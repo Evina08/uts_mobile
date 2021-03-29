88cd594151d7c8eab480a8a4a40776dc17847671
Evina <64392104+Evina08@users.noreply.github.com>
2021-03-29 19:23:32 +0700

    widget form book

--- lib/formBook.dart
+++ lib/formBook.dart
@@ -25,7 +25,158 @@ class FormBookState extends State<FormBook> {
       namaBukuController.text = book.namaBuku;
       penerbitBukuController.text = book.penerbitBuku;
       penulisBukuController.text = book.penulisBuku;
-      jumlahBukuController.text = book.jumlahBuku;
+      jumlahBukuController.text = book.jumlahBuku.toString();
     }
+    //membuat widget
+    return Scaffold(
+        appBar: AppBar(
+          title: book == null ? Text('Tambah') : Text('Edit'),
+          leading: Icon(Icons.keyboard_arrow_left),
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+          child: ListView(
+            children: <Widget>[
+              // kolom form kategori buku
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                child: TextField(
+                  controller: kategoriBukuController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: 'Kategori Buku',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    //
+                  },
+                ),
+              ),
+              // kolom form nama buku
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                child: TextField(
+                  controller: namaBukuController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Nama Buku',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    //
+                  },
+                ),
+              ),
+              //kolom form Penerbit buku
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                child: TextField(
+                  controller: penerbitBukuController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Penerbit Buku',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    //
+                  },
+                ),
+              ),
+              //kolom form penulis buku
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                child: TextField(
+                  controller: penulisBukuController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Penulis Buku',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    //
+                  },
+                ),
+              ),
+              //kolom form jumlah buku
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                child: TextField(
+                  controller: jumlahBukuController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Jumlah Buku',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    //
+                  },
+                ),
+              ),
+              // tombol button
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    // tombol simpan
+                    Expanded(
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        textColor: Theme.of(context).primaryColorLight,
+                        child: Text(
+                          'Save',
+                          textScaleFactor: 1.5,
+                        ),
+                        onPressed: () {
+                          if (book == null) {
+                            // tambah data
+                            book = Book(namaBukuController.text,
+                                int.parse(jumlahBukuController.text));
+                          } else {
+                            // ubah data
+                            book.kategoriBuku = kategoriBukuController.text;
+                            book.namaBuku = namaBukuController.text;
+                            book.penerbitBuku = penerbitBukuController.text;
+                            book.penulisBuku = penulisBukuController.text;
+                            book.jumlahBuku =
+                                int.parse(jumlahBukuController.text);
+                          }
+                          // kembali ke layar sebelumnya dengan membawa objek
+                          Navigator.pop(context, book);
+                        },
+                      ),
+                    ),
+                    Container(
+                      width: 5.0,
+                    ),
+                    // tombol batal
+                    Expanded(
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        textColor: Theme.of(context).primaryColorLight,
+                        child: Text(
+                          'Cancel',
+                          textScaleFactor: 1.5,
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
