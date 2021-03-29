4a3e28b8d0a10df16a36e5420060b9e7490dafbc
Evina <64392104+Evina08@users.noreply.github.com>
2021-03-29 14:14:22 +0700

    Pembuatan atribut table book

---
+++ lib/book.dart
@@ -0,0 +1,9 @@
+class book {
+  //pembuatan atribut pada table book
+  int _idBuku;
+  String _kategoriBuku;
+  String _namaBuku;
+  String _penerbitBuku;
+  String _penulisBuku;
+  int _jumlahBuku;
+}
