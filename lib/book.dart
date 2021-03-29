c9ab43ccb53a6baf927f99c95bfc4998951b0019
Evina <64392104+Evina08@users.noreply.github.com>
2021-03-29 14:19:08 +0700

    pembuatan konstruktor

--- lib/book.dart
+++ lib/book.dart
@@ -1,4 +1,4 @@
-class book {
+class Book {
   //pembuatan atribut pada table book
   int _idBuku;
   String _kategoriBuku;
@@ -6,4 +6,16 @@ class book {
   String _penerbitBuku;
   String _penulisBuku;
   int _jumlahBuku;
+  //konstruktor default
+  Book(this._idBuku, this._kategoriBuku, this._namaBuku, this._penerbitBuku,
+      this._penulisBuku);
+  // konstruktor konversi dari Map ke Item
+  Book.fromMap(Map<String, dynamic> map) {
+    this._idBuku = map['idBuku'];
+    this._kategoriBuku = map['nkategoriBuku'];
+    this._namaBuku = map['namaBuku'];
+    this._penerbitBuku = map['penerbitBuku'];
+    this._penulisBuku = map['penulisBuku'];
+    this._jumlahBuku = map['jumlahBuku'];
+  }
 }
