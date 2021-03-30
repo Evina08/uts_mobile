f2c6f65ed22f4b90a065652da6a0edb185e48c18
Evina <64392104+Evina08@users.noreply.github.com>
2021-03-30 09:01:55 +0700

    membuat home tampilan awal

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {}
+}
