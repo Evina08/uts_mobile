0a38fc41c6fc3ec1b720f9b34ac805ae3d7dd463
Evina <64392104+Evina08@users.noreply.github.com>
2021-04-02 21:24:43 +0700

    Final result dan pembuatan tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 //kode utama Aplikasi tampilan awal
 import 'package:flutter/material.dart';
-import 'home.dart'; //package letak folder Anda
+import 'package:uts_mobile/homeAnggota.dart';
+import 'package:uts_mobile/homeBook.dart';
+ //package letak folder Anda
 
 void main() => runApp(MyApp());
 
@@ -13,7 +15,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: Home(),
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text("Admin Perpustakaan"),
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(
+                    icon: new Icon(
+                      Icons.book,
+                    ),
+                    text: "Buku",
+                  ),
+                  Tab(
+                    icon: new Icon(
+                      Icons.people,
+                    ),
+                    text: "Anggota",
+                  ),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                HomeBook(HomeBook),
+                HomeAnggota(HomeAnggota),
+              ],
+            )),
+      ),
     );
   }
 }
